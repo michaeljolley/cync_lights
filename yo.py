4edee7d76d51488bf0f64ced283d5be52ff83c91
Michael Jolley <mike@baldbeardedbuilder.com>
2024-03-27 10:57:06 -0500

    weeeeeeeee

--- yo.py
+++
@@ -1,9 +0,0 @@
-
-# write to console "hello world"
-print("hello world")
-
-API_AUTH = "https://api.gelighting.com/v2/user_auth"
-API_REQUEST_CODE = "https://api.gelighting.com/v2/two_factor/email/verifycode"
-API_2FACTOR_AUTH = "https://api.gelighting.com/v2/user_auth/two_factor"
-API_DEVICES = "https://api.gelighting.com/v2/user/{user}/subscribe/devices"
-API_DEVICE_INFO = "https://api.gelighting.com/v2/product/{product_id}/device/{device_id}/property"
